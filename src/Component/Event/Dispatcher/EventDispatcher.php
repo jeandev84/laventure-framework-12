2b2b00d04fc080d9c6d13b0ebf1ff5b85dfaaee7
Jean Claude <jeanyao@ymail.com>
2024-01-22 12:45:24 +0500

    Events

---
+++ src/Component/Event/Dispatcher/EventDispatcher.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+
+namespace Laventure\Component\Event\Dispatcher;
+
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+use Psr\EventDispatcher\StoppableEventInterface;
+
+/**
+ * EventDispatcher
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  Laventure\Component\Event\Dispatcher
+ */
+class EventDispatcher implements EventDispatcherInterface
+{
+
+
+    /**
+     * @param ListenerProviderInterface $listenerProvider
+    */
+    public function __construct(protected ListenerProviderInterface $listenerProvider)
+    {
+    }
+
+
+
+    /**
+     * @inheritDoc
+    */
+    public function dispatch(object $event)
+    {
+        if ($event instanceof StoppableEventInterface && $event->isPropagationStopped()) {
+            return $event;
+        }
+
+        foreach ($this->listenerProvider->getListenersForEvent($event) as $listener) {
+            $listener($event);
+        }
+
+        return $event;
+    }
+}
---
+++ src/Component/Event/Subscriber/EventSubscriberInterface.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Laventure\Component\Event\Subscriber;
+
+
+/**
+ * EventSubscriberInterface
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  Laventure\Component\Event\Subscriber
+ */
+interface EventSubscriberInterface
+{
+    /**
+     * Returns subscribed events
+     *
+     * @return array
+    */
+    public function getSubscribedEvents(): array;
+}
