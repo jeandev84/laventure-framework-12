adde553082a0695df6dd0ce6979b19144de778ac
Jean Claude <jeanyao@ymail.com>
2024-01-22 21:21:39 +0500

    Fixers

---
+++ tests/phpunit/Component/Templating/Renderer/RendererTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnitTest\Component\Templating\Renderer;
+
+use Laventure\Component\Templating\Renderer\Renderer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * RendererTest
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  PHPUnitTest\Component\Templating\Renderer
+ */
+class RendererTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+}
