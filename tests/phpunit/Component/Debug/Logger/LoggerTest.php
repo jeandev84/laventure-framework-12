c98a7d900ac6a7c41665053a6dbb71a89a674927
Jean Claude <jeanyao@ymail.com>
2024-01-26 18:39:06 +0500

    Add InvalidLogLevelException

---
+++ tests/phpunit/Component/Debug/Logger/LoggerTest.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace PHPUnitTest\Component\Debug\Logger;
+
+use Laventure\Component\Debug\Logger\Logger;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * LoggerTest
+ *
+ * @author Jean-Claude <jeanyao@ymail.com>
+ *
+ * @license https://github.com/jeandev84/laventure-framework/blob/master/LICENSE
+ *
+ * @package  PHPUnitTest\Component\Debug\Logger
+ */
+class LoggerTest extends TestCase
+{
+
+}
